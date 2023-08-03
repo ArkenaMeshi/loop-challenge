2fcfc71dda777cb2e535cb37f8ee406a5b8d74bc
ArkenaMeshi <arkena-meshi@hotmail.com>
2023-08-03 16:48:02 +0200

    loop challenge

---
+++ index.js
@@ -0,0 +1,30 @@
+for (let index = 1; index <= 20; index++) {
+    if( index%2 !==0)
+    {
+        console.log(index)
+    }
+}
+for (let index = 100; index >=0; index--) {
+    if( index%3==0)
+    {
+        console.log(index)
+    }
+}
+let value =4 ; 
+while ( value >=-3.5) {
+    console.log(value);
+    value-=1.5;
+}
+var sum=0;
+for(let i=1; i<=100;i++){
+    sum= sum + i ;
+    
+}
+console.log(sum)
+
+var product=1;
+for(let i=1; i<=12;i++){
+    product= product * i ;
+    
+}
+console.log(product)
